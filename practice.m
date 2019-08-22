88427dc353d7a90ce02cdb1bc258007b2d19e5a4
Lee0326 <lcyfly1@163.com>
2019-08-22 10:16:50 +0800

    Test
    New practice

--- practice.m
+++ practice.m
@@ -1,9 +1,10 @@
 clear all;
 clc;
+
 % The range resolution = 1m
 % The speed of light c = 3*10^8
 
-
+%% Range estimation
 % TODO : Find the Bsweep of chirp for 1 m resolution
 c = 3*10^8;
 delta_r = 1;
@@ -19,3 +20,50 @@ calculated_range = c*Ts*beat_freq/(2*Bsweep);  %range calculation
 
 %display the calculate range
 disp(calculated_range);
+
+%% Doppler Velocity Calculation
+c = 3*10^8;         %speed of light
+frequency = 77e9;   %frequency in Hz
+
+% TODO : Calculate the wavelength
+wavelength = c/frequency;
+
+% TODO : Define the doppler shifts in Hz using the information from above 
+doppler_shift = [3e3 -4.5e3 11e3 -3e3];
+
+% TODO : Calculate the velocity of the targets  fd = 2*vr/lambda
+
+Vr = doppler_shift*wavelength/2;
+
+% TODO: Display results
+disp(Vr);
+
+%% FFT Practice
+Fs = 1000;            % Sampling frequency                    
+T = 1/Fs;             % Sampling period       
+L = 1500;             % Length of signal
+t = (0:L-1)*T;        % Time vector
+
+% TODO: Form a signal containing a 77 Hz sinusoid of amplitude 0.7 and a 43Hz sinusoid of amplitude 2.
+S = ?
+
+% Corrupt the signal with noise 
+X = S + 2*randn(size(t));
+
+% Plot the noisy signal in the time domain. It is difficult to identify the frequency components by looking at the signal X(t). 
+plot(1000*t(1:50) ,X(1:50))
+title('Signal Corrupted with Zero-Mean Random Noise')
+xlabel('t (milliseconds)')
+ylabel('X(t)')
+
+% TODO : Compute the Fourier transform of the signal. 
+
+% TODO : Compute the two-sided spectrum P2. Then compute the single-sided spectrum P1 based on P2 and the even-valued signal length L.
+
+
+% Plotting
+f = Fs*(0:(L/2))/L;
+plot(f,P1) 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
